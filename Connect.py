99a682914d1a837227190c8b5c78927690e8a927
Ondrej Skopek <skopekondrej@gmail.com>
2013-07-17 22:36:36 +0200

    NOTWORKING commit --> intermediate step to creating jabber implementation, connect command

--- Connect.py
+++ Connect.py
@@ -15,12 +15,66 @@ if sys.version_info < (3, 0):
 else:
     raw_input = input
 
-class CmdInterface(cmd.Cmd): 
+class Connect(cmd.Cmd): 
 
-    def __init__(self):
+    def __init__(self, args):
         cmd.Cmd.__init__(self)
         self.prompt = "> "
-        self.intro  = "\nWelcome to CryptoIM!\n"  ## defaults to None
+        
+        # Setup the command line arguments.
+    	optp = OptionParser()
+
+    	# Output verbosity options.
+    	optp.add_option('-q', '--quiet', help='set logging to ERROR',
+                    action='store_const', dest='loglevel',
+                    const=logging.ERROR, default=logging.INFO)
+    	optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    	optp.add_option('-v', '--verbose', help='set logging to COMM',
+                    action='store_const', dest='loglevel',
+                    const=5, default=logging.INFO)
+
+    	# JID and password options.
+    	optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    	optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+
+    	opts, args = optp.parse_args()
+
+    	# Setup logging.
+    	logging.basicConfig(level=opts.loglevel,
+                        format='%(levelname)-8s %(message)s')
+
+    	if opts.jid is None:
+        	opts.jid = raw_input("Username: ")
+    	if opts.password is None:
+        	opts.password = getpass.getpass("Password: ")
+
+    	# Setup the EchoBot and register plugins. Note that while plugins may
+    	# have interdependencies, the order in which you register them does
+    	# not matter.
+    	self.xmpp = XMPPClient(opts.jid, opts.password)
+    	self.xmpp.register_plugin('xep_0030') # Service Discovery
+    	self.xmpp.register_plugin('xep_0004') # Data Forms
+    	self.xmpp.register_plugin('xep_0060') # PubSub
+    	self.xmpp.register_plugin('xep_0199') # XMPP Ping
+
+
+    	# Connect to the XMPP server and start processing XMPP stanzas.
+    	if xmpp.connect():
+        # If you do not have the dnspython library installed, you will need
+        # to manually specify the name of the server if it does not match
+        # the one in the JID. For example, to use Google Talk you would
+        # need to use:
+        #
+        # if xmpp.connect(('talk.google.com', 5222)):
+        #     ...
+        	xmpp.process(block=False)
+        	print("Done")
+    	else:
+        	print("Unable to connect.")
 
 
     ## Command definitions ##
@@ -45,8 +99,9 @@ class CmdInterface(cmd.Cmd):
     	"""
     	TODO HELP ON CONNECT
     	"""
-    	connect = Connect(args)
-    	connect.cmdloop()
+    	
+    	
+    	
 
     def do_help(self, args):
         """Get help on commands
