958b383f6be193e9e0be5772b1a386e61d2236db
Ondrej Skopek <skopekondrej@gmail.com>
2013-12-30 22:49:44 +0100

    Added basic directory structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='CryptoIM',
+    version='0.1.0dev',
+    packages=['crypto_im',],
+    license='TODO license',
+    long_description=open('README.adoc').read(),
+)
