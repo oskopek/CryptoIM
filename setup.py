b874ab2f4221eab3b43facba63cc2619d5cf642f
Ondrej Skopek <skopekondrej@gmail.com>
2013-12-30 23:41:01 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 from setuptools import setup, find_packages
 
 setup(
     name = "CryptoIM",
     version = "0.1.0dev",
-    packages= find_packages(),
+    packages = find_packages(exclude=["Test*"]),
+    install_requires = ["docutils >= 0.3"],
     license = "TODO license",
     long_description = open("README.adoc").read(),
 )
