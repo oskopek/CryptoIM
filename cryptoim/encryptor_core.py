d8df46d91a8984c4e91e43e7d52256b3b87ff18b
mators11 <mr.mators11@gmail.com>
2014-01-01 11:45:17 +0100

    Added message splitter

--- cryptoim/encryptor_core.py
+++ cryptoim/encryptor_core.py
@@ -33,21 +33,48 @@ key = string (256 bytes)
         return extendedkey
 
     def RoundKeySeparator(extendedkey):
+    """
+    Returns list of 16 matrices, these are 128 bit roundkeys used for encryption,
+    from there 16 matrices will be only 14 used. For opimization purpose was used
+    index k instead of another cycle.
+    """
         k = 0
         roundkeys = []
-        for i in range(16):
+        for k in range(16):
             roundkey = [[],[],[],[]] #Matrix (list of lists)
             for i in range(4):
-                for j in range(4):
+                for j in range(4): 
                     hexadecimal = int(hex(ord(extendedkey[k])),16) #Converts letter to decimal number
-                    roundkey[i].append(hexadecimal)
+                    roundkey[i].append(hexadecimal)#Appends 4 numbers into each row of matrix
                     k += 1
             roundkeys.append(roundkey)
         return roundkeys
 
-"""
-Returns list of 16 matrices, these are 128 bit roundkeys used for encryption,
-from there 16 matrices will be only 14 used. For opimization purpose was used
-index k instead of another cycle.
-"""
+    def SplitMessage(plaintext):
+    """
+    Splits message into 128 bits (16 characters) chunks and each of these
+    chunks is then transformed into matrix with decimal value. These matrices
+    are stored into list, creating a list of matrices.
+    """
+    messsage_chunks = []
+    message_chunk = ''
+    for character in plaintext:
+        message_chunk += character
+        if len(message_chunk) == 16: #After 16 characters appends 1 chunk into a list of chunks
+            message_chunks.append(message_chunk)
+            message_chunk = ''
+    messages = []
+    for i in range(len(message_chunks)):
+        matrix = [[],[],[],[]]
+        for j in range(4):
+            for k in range(4):
+                number = int(hex(ord(message_chunks[4*i+j])),16) #Cool way to iterate and transform at the same time
+                matrix[j].append(number)
+        messages.append(matrix)
+    return messages
+    """
+    TODO: Check if this works as expected
+    """
+            
+        
     
