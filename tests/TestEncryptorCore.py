34bd4c309c4d3e0931d510a8d628fc0210ba8ad1
Ondrej Skopek <skopekondrej@gmail.com>
2014-01-03 01:32:13 +0100

    Emptied out decryptor, fixed up encryptor method and variable names

--- tests/TestEncryptorCore.py
+++ tests/TestEncryptorCore.py
@@ -20,11 +20,11 @@
 from cryptoim import encryptor_core
 from nose.tools import ok_, eq_
 
-def testEncrypt():
+def test_encrypt():
     """
-        Test for encryptor_core.Encrypt
+        Test for encryptor_core.encrypt
     """
-    Encrypt = encryptor_core.Encrypt
+    encrypt = encryptor_core.encrypt
 
     message = "test"
 
@@ -40,19 +40,19 @@ def testEncrypt():
     key = rand_str(256)
     eq_(len(key), 256)
 
-    #ok_(len(Encrypt(message, key)) != 0, "Length wasn't supposed to be 0")
+    #ok_(len(encrypt(message, key)) != 0, "Length wasn't supposed to be 0")
 
-def testGMul():
+def test_g_mul():
     """
-        Test for encryptor_core.__GMul
+        Test for encryptor_core.__g_mul
     """
-    GMul = encryptor_core.__GMul
+    g_mul = encryptor_core.__g_mul
 
-    eq_(GMul(0x00,0x01),0x00)
-    eq_(GMul(0x00,0xFF),0x00)
-    eq_(GMul(0x01,0xAB),0xAB)
+    eq_(g_mul(0x00,0x01),0x00)
+    eq_(g_mul(0x00,0xFF),0x00)
+    eq_(g_mul(0x01,0xAB),0xAB)
     print(0x02)
     print(0xFF)
     print(0x02 * 0xFF)
-    print(GMul(0x02,0xFF))
-    # ok_(GMul(0x02,0xFF)<=0xFF,"This thing does not work") # out-commented, because it doesn't work and would break the build
+    print(g_mul(0x02,0xFF))
+    # ok_(g_mul(0x02,0xFF)<=0xFF,"This thing does not work") # out-commented, because it doesn't work and would break the build
