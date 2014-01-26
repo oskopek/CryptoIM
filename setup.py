d804ab450e167c03a134d717fca2b83d01657467
Ondrej Skopek <skopekondrej@gmail.com>
2014-01-26 22:47:12 +0100

    Pushing version [skip ci]

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'CryptoIM',
-    version = '0.1.0',
+    version = '0.2.0dev',
     author = 'CryptoIM Development Team',
     author_email = 'skopekondrej@gmail.com',
     packages = find_packages(exclude=['tests']),
